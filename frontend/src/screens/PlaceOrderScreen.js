00654d80757aadd35961adfc260e0074124f9429
Mhaelgaidin <d_hooper@hotmail.co.uk>
2020-10-26 11:01:53 +0000

    removed unused imports

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,13 +1,5 @@
 import React, { useState } from 'react'
-import {
-  Button,
-  Row,
-  Col,
-  CardColumns,
-  ListGroup,
-  Image,
-  Card,
-} from 'react-bootstrap'
+import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
