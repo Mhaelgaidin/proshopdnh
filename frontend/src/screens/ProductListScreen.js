3bd866c3139adbda7b406c597a7119c6abdfb5ea
Mhaelgaidin <d_hooper@hotmail.co.uk>
2020-10-27 15:48:07 +0000

    product edit screen

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -43,7 +43,7 @@ const ProductListScreen = ({ history, match }) => {
     }
 
     if (successCreate) {
-      history.push(`/admin/products/${createdProduct._id}`)
+      history.push(`/admin/products/${createdProduct._id}/edit`)
     } else {
       dispatch(listProducts())
     }
@@ -107,7 +107,7 @@ const ProductListScreen = ({ history, match }) => {
                 <td>{product.category}</td>
                 <td>{product.brand}</td>
                 <td>
-                  <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                  <LinkContainer to={`/admin/products/${product._id}/edit`}>
                     <Button variant='light' className='btn-sm'>
                       <i className='fas fa-edit'></i>
                     </Button>
